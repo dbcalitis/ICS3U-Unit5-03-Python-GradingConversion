d25a6ad346a30f435c0e54a296994eebfdfafe65
Daria Bernice <daria.bernice.calitis@mths.ca>
2021-10-14 16:41:36 +0000

    added grading_level.py

---
+++ grading_level.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+
+# Created by: Daria Bernice Calitis
+# Created on: Oct 2021
+# This program converts Ontario's Grading to percentage
+
+
+def convert_to_percentage(grade_level):
+    if grade_level == "4+":
+        percentage = 97.0
+    elif grade_level == "4":
+        percentage = 90.5
+    elif grade_level == "4-":
+        percentage = 83.0
+    elif grade_level == "3+":
+        percentage = 78.0
+    elif grade_level == "3":
+        percentage = 74.5
+    elif grade_level == "3-":
+        percentage = 71.0
+    elif grade_level == "2+":
+        percentage = 68.0
+    elif grade_level == "2":
+        percentage = 64.5
+    elif grade_level == "2-":
+        percentage = 61.0
+    elif grade_level == "1+":
+        percentage = 58.0
+    elif grade_level == "1":
+        percentage = 54.5
+    elif grade_level == "1-":
+        percentage = 51.0
+    elif grade_level == "R":
+        percentage = 24.5
+    else:
+        percentage = -1
+    return percentage
+
+
+def main():
+    # This function is the main function
+    # input
+    user_input = input("Enter a grade level (Ontario rubric): ")
+
+    # call function
+    print(
+        "The percentage of {0} is {1}%.".format(
+            user_input, convert_to_percentage(user_input)
+        )
+    )
+
+    print("\nDone.")
+
+
+if __name__ == "__main__":
+    main()
